5ffb1c4e0b266ac2676cc605c3ac44995bbb128a
ED <eddie@ftwcoin.io>
2020-08-12 00:26:07 -0700

    Updates

---
+++ precache-manifest.fcca11d42d1741ee0548dbb8f8ad7ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ee61a3baffab8cc661d4639aa5db35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a352b1b937219ff0873b",
+    "url": "/static/css/main.0c8b40f5.chunk.css"
+  },
+  {
+    "revision": "9321d56b66c7c56a463e",
+    "url": "/static/js/2.38d34383.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.38d34383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a352b1b937219ff0873b",
+    "url": "/static/js/main.467ad0aa.chunk.js"
+  },
+  {
+    "revision": "3201c5af7df2b3a62a73",
+    "url": "/static/js/runtime-main.f6000d1a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ftwcoin.io/precache-manifest.17ad3c58c0f861168c90773e2ebcc11b.js"
+  "/precache-manifest.fcca11d42d1741ee0548dbb8f8ad7ef2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ftwcoin.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
